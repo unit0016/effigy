ec93b3e10c5495b20cbd6d4f111c7afd50330521
itsmeow <itsmeow@itsmeow.dev>
2025-03-13 19:59:12 -0500

    IconForge: rust-g GAGS (250x faster edition) (#89590)
    ## About The Pull Request
    
    Offloads GAGS generation to rust-g IconForge.
    
    **Key Notes**
    
    - The builtin GAGS editor still uses the 'legacy' generation to allow
    for debugging.
    - Does not support `color_matrix` layer type, which is currently not
    used by any GAGS configs. Will do nothing if used.
    - Does not support `or` blending mode, which is currently not used by
    any GAGS configs. Will error if used.
    - Has some 'quirks' compared to BYOND when it comes to mixing icon
    states with different dir/frame amounts. BYOND will just silently handle
    these and it's basically undefined behavior because what should you
    expect BYOND to do? IconForge will spit errors out instead. So this PR
    also fixes a few of those cases.
    
    Functions by writing output to `tmp/gags/gags-[...].dmi`, copying that
    output into the RSC and assigning the file object to `icon`.
    
    Saves ~1.7s init by reducing worst-case GAGS icon generation from 250ms
    to 1ms.
    
    Also optimizes `icon_exists` by using `rustg_dmi_icon_states` for file
    icons, saving ~60ms. Would have more savings if not for json_decode as
    well as DMI parsing in rust being somewhat slow. Perhaps having
    `rustg_dmi_icon_states` share a cache with IconForge could reduce this
    cost, however I'd still recommend limiting these tests to unit tests
    (https://github.com/tgstation/dev-cycles-initiative/issues/34),
    especially for GAGS configs. I'm not sure they're worth 700ms.
    
    Saves another ~400ms by replacing `md5asfile` with `rustg_hash_file` in
    `/datum/greyscale_config/proc/Refresh`
    
    Savings are likely even higher when combined with #89478, due to
    spritesheets sharing a parsed DMI cache with GAGS. This means GAGS will
    spend less time parsing icons synchronously and can generate output
    faster. Tracy tests with this combo seem to yield ~2sec savings instead
    of ~1.7sec
    
    Total savings: ~2.16sec to ~2.46sec
    
    - Ports https://github.com/BeeStation/BeeStation-Hornet/pull/10455
    - Resolves https://github.com/tgstation/dev-cycles-initiative/issues/9
    
    
    
    ## Why It's Good For The Game
    
    GAGS go zoooom
    
    <details>
    <summary>GAGS Working Ingame</summary>
    
    
    ![image](https://github.com/user-attachments/assets/28df25a5-bdf0-4a63-a6cf-b15f85467b23)
    
    
    ![image](https://github.com/user-attachments/assets/6a9dab46-5814-47ea-ad9b-f5ec84a6333d)
    
    </details>
    
    <details>
    <summary>GetColoredIconByType</summary>
    
    
    ![image](https://github.com/user-attachments/assets/1698729f-1101-4413-bfb3-0922b389c347)
    
    </details>
    
    <details>
    <summary>icon_exists</summary>
    
    
    ![image](https://github.com/user-attachments/assets/9e72c6aa-287f-4ce3-8dbe-9d3bebf3a762)
    
    </details>
    
    
    <details>
    <summary>Refresh</summary>
    
    
    ![image](https://github.com/user-attachments/assets/18c15073-a294-4db6-bdd0-cdc7d8682221)
    
    </details>
    
    
    ## Changelog
    :cl:
    tweak: Optimized GAGS using rust-g IconForge, reducing worst-case
    generation time to 1ms
    /:cl:

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -11,6 +11,7 @@ fi
 
 mkdir -p \
     $1/_maps \
+    $1/code/datums/greyscale/json_configs \
     $1/data/spritesheets \
     $1/icons \
     $1/local/icons \
@@ -26,6 +27,7 @@ fi
 
 cp tgstation.dmb tgstation.rsc $1/
 cp -r _maps/* $1/_maps/
+cp -r code/datums/greyscale/json_configs/* $1/code/datums/greyscale/json_configs/
 cp -r icons/* $1/icons/
 cp -r local/icons/* $1/local/icons/ # EffigyEdit Add
 cp -r sound/runtime/* $1/sound/runtime/
